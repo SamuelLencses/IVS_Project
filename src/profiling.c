96073c474aa5a1e9d626de2292c051453a9fa417
xpauli08 <xpauli08@stud.fit.vutbr.cz>
2022-04-25 14:46:57 +0200

    Slightly changed the structure

--- src/profiling.c
+++ src/profiling.c
@@ -1,25 +1,23 @@
 #include <stdio.h>
-#include <math.h>
 #include "math_lib.h"
 
-double avg(int input[], int size);
-double squareSum(int input[], int size);
+int getInput(int *input); // Loads values into array input and returns its size
+
+double avg(int input[], int size); // Count the average
+double squareSum(int input[], int size); // Count the sum of square roots of elements in array input
+double getAns(int input[], int size);
 
 int main() {
 
     // Max size of 1000 integers
     int input[1000];
-    int size = 0;
-
-    // Load the numbers into array
-    while (scanf("%d", &input[size]) != EOF) {
-        size++;
-    }
+    int size = getInput(input);
 
     // The actual result
-    double result = root(mul(dv(1, size-1).ans, sub(squareSum(input, size), mul(size, mul(avg(input, size), avg(input, size)).ans).ans).ans).ans, 2).ans;
+    //double result = root(mul(dv(1, size-1).ans, sub(squareSum(input, size), mul(size, mul(avg(input, size), avg(input, size)).ans).ans).ans).ans, 2).ans;
 
-    printf("%f\n", result);
+    // Print out the result
+    printf("%f\n", getAns(input, size));
 
     return 0;
 }
@@ -49,4 +47,20 @@ double squareSum(int numbers[], int size) {
     }
 
     return res;
+}
+
+int getInput(int *input)
+{
+    int size = 0;
+    while (scanf("%d", &input[size]) != EOF)
+    {
+        size++;
+    }
+    return size;
+}
+
+double getAns(int input[], int size)
+{
+    double result = root(mul(dv(1, size-1).ans, sub(squareSum(input, size), mul(size, mul(avg(input, size), avg(input, size)).ans).ans).ans).ans, 2).ans;
+    return result;
 }
