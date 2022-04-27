fd1a1eeb2f1aafafb505b6046deff28f949dc7c7
xpauli08 <xpauli08@stud.fit.vutbr.cz>
2022-04-28 00:26:52 +0200

    Added doxygen comments

--- src/math_lib.c
+++ src/math_lib.c
@@ -1,7 +1,31 @@
+/***************************************************************************************************************************************
+ * Name of the project: IVS Project II
+ * File: math_lib.c
+ * Date: 2022-03-26
+ * Last change: 2022-04-27
+ * Author: Ondřej Novenilka (xnovel00)
+ * 
+ * Description: Library containing basic math functions 
+ * 
+ * *************************************************************************************************************************************
+/**
+ * @file math_lib.c
+ * 
+ * @brief Library containing basic math functions
+ * @author Ondřej Novelinka (xnovel00)
+ *
+ */
+
 #include "math_lib.h"
 #include <math.h>
 
-// Soucet
+/**
+ * @brief Sum of two numbers
+ * 
+ * @param a First number to be added
+ * @param b Second number to be added
+ * @return Sum of a and b
+ */
 result_d add(long double a, long double b) {
     result_d res;
     res.ans = a + b;
@@ -9,7 +33,13 @@ result_d add(long double a, long double b) {
     return res;
 }
 
-// Rozdil
+/**
+ * @brief Subtraction of two numbers
+ * 
+ * @param a First number that is being substracted from
+ * @param b Second number to subtract from the first
+ * @return Subtraction of a and b
+ */
 result_d sub(long double a, long double b) {
     result_d res;
     res.ans = a - b;
@@ -17,7 +47,13 @@ result_d sub(long double a, long double b) {
     return res;
 }
 
-// Nasobeni
+/**
+ * @brief Multiplication of two numbers
+ * 
+ * @param a First number to be multiplied
+ * @param b Second number to be multiplied
+ * @return Multiplication of a and b
+ */
 result_d mul(long double a, long double b) {
     result_d res;
     res.ans = a * b;
@@ -25,7 +61,14 @@ result_d mul(long double a, long double b) {
     return res;
 }
 
-// Deleni
+/**
+ * @brief Division of two numbers
+ * 
+ * @param a First number - dividend
+ * @param b Second number - divisor
+ * @return Division of a and b
+ * @exception If b is equal to 0, dv(a, b).fail == 1
+ */
 result_d dv(long double a, long double b) {
     result_d res;
     if (b == 0)
@@ -40,7 +83,13 @@ result_d dv(long double a, long double b) {
     return res;
 }
 
-// Faktorial
+/**
+ * @brief Factorial of a number
+ * 
+ * @param a Number which factorial is calculated
+ * @return Factorial of number a
+ * @exception If a is less than 0, fact(a).fail == 1
+ */
 result_i fact(int a) {
     result_i res;
 
@@ -59,7 +108,14 @@ result_i fact(int a) {
     return res;
 }
 
-// Mocnina
+/**
+ * @brief Power of a number
+ * 
+ * @param x First number, base
+ * @param n Second number, exponent
+ * @return x to the power of n
+ * @exception If n is less than 0, power(x, n).fail == 1
+ */
 result_d power(long double x, int n) {
     result_d res;
     
@@ -90,7 +146,14 @@ result_d power(long double x, int n) {
     return res;
 }
 
-// Odmocnina
+/**
+ * @brief Root of a number
+ * 
+ * @param x First number, base
+ * @param n Second number, exponent
+ * @return Nth root of number x
+ * @exception If n is less than 1, power(x, n).fail == 1
+ */
 result_d root(long double x, int n) {
     result_d res;
 
@@ -115,7 +178,12 @@ result_d root(long double x, int n) {
 
 }
 
-// Absolutni hodnota
+/**
+ * @brief Absolute value
+ * 
+ * @param a Number which absolute value will be calculated
+ * @return Absolute value of a
+ */
 result_d absolute(long double a) {
     result_d res;
     if (a < 0)
